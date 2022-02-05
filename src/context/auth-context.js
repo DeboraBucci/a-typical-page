7000f0f7345a520d1269c3cec88b4d80e384e272
Debora Bucci <dbucci@protonmail.com>
2022-02-04 23:38:08 -0300

    create a dummy 'onLogout' function to help with auto completion when coding

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -3,6 +3,7 @@ import React from "react";
 // Can also be a string: React.createContext("");
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
 });
 
 export default AuthContext;
