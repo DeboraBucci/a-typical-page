21983b73460a6afd2318c1a2e77a4299031bdeb2
Debora Bucci <dbucci@protonmail.com>
2022-02-05 00:16:41 -0300

    move all the authentication logic to the 'auth-context.js' file so the code is cleaner

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../context/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT")
@@ -32,6 +33,8 @@ const Login = (props) => {
     passwordReducer,
     initState
   );
+
+  const authCtx = useContext(AuthContext);
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -66,7 +69,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
